ab429d18d6381ab909bc98a753843246ce50aed2
Adam Coulon <adam@rollforward.net>
2017-07-20 07:37:54 -0500

    insults now rotate

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
-var first_term = ["Butterface", "Communist", "Creepy", "Democrat", "Dicknose", "Drug-Loving", "Elitist", "Goat", "Idiotic", "Insecure", "Lazy", "Pie-Eating", "Pompous", "Racist", "Slimy", "Slutty", "Smelly", "Stupid", "Tone Deaf", "Ugly", "White Trash"];
-
-var second_term = ["Ass", "Ball", "Bitch", "Boner", "Butt", "Cock", "Crotch", "Dick", "Douche", "Fuck", "Nut", "Prick", "Rectum", "Shart", "Shit", "Slut", "Sperm", "Sphincter", "Taint", "Turd", "Twat"];
-
-var third_term = ["Bag", "Balloon", "Biscuit", "Blossom", "Box", "Canoe", "Captain", "Clown", "Dragon", "Goblin", "Hammer", "Hound", "Jockey", "Knob", "Monster", "Nazi", "Pilot", "Pirate", "Socket", "Sucker", "Waffle"]
+var terms = {
+  first: ["Butterface", "Communist", "Creepy", "Democrat", "Dicknose", "Drug-Loving", "Elitist", "Goat", "Idiotic", "Insecure", "Lazy", "Pie-Eating", "Pompous", "Racist", "Slimy", "Slutty", "Smelly", "Stupid", "Tone Deaf", "Ugly", "White Trash"],
+  second: ["Ass", "Ball", "Bitch", "Boner", "Butt", "Cock", "Crotch", "Dick", "Douche", "Fuck", "Nut", "Prick", "Rectum", "Shart", "Shit", "Slut", "Sperm", "Sphincter", "Taint", "Turd", "Twat"],
+  third: ["Bag", "Balloon", "Biscuit", "Blossom", "Box", "Canoe", "Captain", "Clown", "Dragon", "Goblin", "Hammer", "Hound", "Jockey", "Knob", "Monster", "Nazi", "Pilot", "Pirate", "Socket", "Sucker", "Waffle"]
+};
 
 function selectTerm(ary) {
   return ary[Math.floor(Math.random()*ary.length)]
 }
 
 function generateInsult() {
-  var first =  selectTerm(first_term);
-  var second =  selectTerm(second_term);
-  var third =  selectTerm(third_term);
+  // get pieces
+  var first =  selectTerm(terms.first);
+  var second =  selectTerm(terms.second);
+  var third =  selectTerm(terms.third);
+
+  // replace with a synonym
+  lookupReplaceSynonym(first, 'first');
+  lookupReplaceSynonym(second, 'second');
+  lookupReplaceSynonym(third, 'third');
+
   return first + " " + second + " " + third + "!";
+}
+
+function lookupReplaceSynonym(term, order) {
+  $.ajax({
+    url: "https://api.datamuse.com/words?rel_syn=" + term,
+    dataType: "json",
+    success: successHandler.bind(null, term, order),
+    error: errorHandler
+  });
+}
+
+function successHandler(term, order, res){
+  if(res && res.length){
+    var chosen = selectTerm(res).word.toUpperCase();
+    var index = terms[order].indexOf(term);
+    terms[order][index] = chosen;
+    console.log("replaced " + term + " with " + chosen);
+  }
+}
+
+function errorHandler(err){
+  console.error(err);
 }
